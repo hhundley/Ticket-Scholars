b8ce26e0881dea046f7148e997a22737c6b12252
hhundley <65366145+hhundley@users.noreply.github.com>
2022-09-27 19:55:18 -0500

    added concertRoutes file and updated imports

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
+// importing necessary modules
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
+// setting up middleware
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
+// exporting router
 module.exports = router;
