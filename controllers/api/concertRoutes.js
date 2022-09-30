75cce79e90a465248adaab6e1c914e7d6cf7f0c5
hhundley <65366145+hhundley@users.noreply.github.com>
2022-09-29 19:53:39 -0500

    imported packages for concert routes, built skeleton for concert get and post routes, and pseudo code for concert booking

--- controllers/api/concertRoutes.js
+++ controllers/api/concertRoutes.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
-const { Events } = require('../../models');
 const withAuth = require('../../utils/auth');
+const { Events } = require('../../models');
 
+// POST route for saved events
+router.post('/', withAuth, async (req, res) => {
+    try {
+    //   add booked events to an array the needs to be stored in another db table. Connect to an account.
+
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+// DELETE route for saved events
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+//    delete specific booked event from an array at a given index
+    if (!savedConcerts) {
+      res.status(404).json({ message: 'Concert not saved' });
+      return;
+    }
+
+    res.status(200).json(savedConcerts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
---
+++ controllers/api/genresRoutes.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Genres } = require('../../models');
