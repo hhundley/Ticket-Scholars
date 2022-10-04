48db7335501c89513c111103e38ccf5890ac18bc
Tee Oni <tzepeda4@gmail.com>
2022-10-03 21:05:25 -0500

    Added more values in DB; Changed Model for Events

--- seeds/seed.js
+++ seeds/seed.js
@@ -9,47 +9,116 @@ const genre = require('../models/Genres')
 
 const eventsForTicketScholars = [
     {
-        "title": "test title 1",
-        "artist": "test artist 1",
+        "title": "Rolling Down A Hill Tour",
+        "artist": "Lumberjack Loggers",
         "show_date": "2022-10-01",
-        "filename": "test_filename1",
-        "description": "test desciption1"
+        "show_time": "7:00PM",
+        "description": "Ticket to Rolling Down a Hill Tour"
     },
 
     {
-        "title": "test title 2",
-        "artist": "test artist 2",
+        "title": "Flapjacks for Dinner World Tour",
+        "artist": "Bacon Pancakes",
         "show_date": "2022-10-02",
-        "filename": "test_filename2",
-        "description": "test desciption2"
+        "show_time": "8:00PM",
+        "description": "Ticket to Flapjacks for Dinner World Tour"
     },
 
     {
-        "title": "test title 3",
-        "artist": "test artist 3",
+        "title": "Summer of 0ur Lyfes Tour",
+        "artist": "BRRRRRR",
         "show_date": "2022-10-03",
-        "filename": "test_filename3",
-        "description": "test desciption3"
+        "show_time": "6:00PM",
+        "description": "Tickets for the Summer of 0ur Lyfes Tour"
     },
 
     {
-        "title": "test title 4",
-        "artist": "test artist 4",
+        "title": "Jazz Hands and Snaps Mean Everything Tour",
+        "artist": "sad court jester",
         "show_date": "2022-10-04",
-        "filename": "test_filename4",
-        "description": "test desciption4"
+        "show_time": "7:00PM",
+        "description": "ticket to Jazz Hands Tour"
     },
 
     {
-        "title": "test title 5",
-        "artist": "test artist 5",
+        "title": "Touring at Its Best Tour",
+        "artist": "Da-tours",
         "show_date": "2022-10-05",
-        "filename": "test_filename5",
-        "description": "test desciption5"
+        "show_time": "6:00PM",
+        "description": "ticket to the touring tour"
+    },
+
+    {
+        "title": "Dreams Tour",
+        "artist": "Pray",
+        "show_date": "2022-10-09",
+        "show_time": "8:00PM",
+        "description": "ticket to the dream tour"
+    },
+
+    {
+        "title": "Seeing Sounds Tour",
+        "artist": "Smores",
+        "show_date": "2022-10-11",
+        "show_time": "7:00PM",
+        "description": "ticket to the seeing sounds tour"
+    },
+
+    {
+        "title": "Dont Tell Ma Tour",
+        "artist": "Dads on Strike",
+        "show_date": "2022-10-11",
+        "show_time": "6:00PM",
+        "description": "ticket to the Dont Tell Ma tour"
+    },
+
+    {
+        "title": "i wanna be someone Tour",
+        "artist": "Nobody",
+        "show_date": "2022-10-25",
+        "show_time": "7:00PM",
+        "description": "ticket to the i wanna be someone tour"
+    },
+
+    {
+        "title": "Bring back the Snack Wrap Tour",
+        "artist": "Mc-lifer",
+        "show_date": "2022-10-05",
+        "show_time": "6:00PM",
+        "description": "ticket to the Snack Wrap tour"
+    },
+
+    {
+        "title": "You at your best when you're crying World Tour",
+        "artist": "Grumpy Cat Lives",
+        "show_date": "2022-10-23",
+        "show_time": "7:00PM",
+        "description": "ticket to the crying world tour"
+    },
+
+    {
+        "title": "Orges are like Onions , ode to Shrek Tour",
+        "artist": "Swamps Stars",
+        "show_date": "2022-10-19",
+        "show_time": "8:00PM",
+        "description": "ticket to the Onion tour"
+    },
+
+    {
+        "title": "NOW THATS WHAT I CALL MUSIC 483 LIVE",
+        "artist": "Untitled Artists",
+        "show_date": "2022-10-20",
+        "show_time": "6:00PM",
+        "description": "ticket to the touring tour"
+    },
+
+    {
+        "title": "Christmas Came Early Tour",
+        "artist": "Sir Kringle",
+        "show_date": "2022-10-25",
+        "show_time": "7:00PM",
+        "description": "ticket to the Christmas Early tour"
     }
-
-
-
 ]
 
 
