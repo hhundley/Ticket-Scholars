e0a0d65b00a262a8bb9a0b6ac0406e3190617628
hhundley <65366145+hhundley@users.noreply.github.com>
2022-10-04 19:58:09 -0500

    redesigning routes to meet new plan

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Events, Genres, User, SavedTickets, Tickets } = require("../models");
+const { Events, Genres, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
@@ -21,65 +21,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-// retreive concert by id and required attributes. Serialize concert as plain js objects
-// Todo: Check render page name when handlebars file is created.
-router.get("/concerts/:id", async (req, res) => {
-  try {
-    const concertData = await Events.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [{ model: Tickets }],
-    });
-    const concert = concertData.get({ plain: true });
-    // render onjects and session id in homepage handlebars template
-    res.render("tickets", {
-      ...concert,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // Require user to be logged in to access the profile page
 router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find user based w/ session id
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
-      // Do we need a different model?
-      include: [{ model: savedTickets }],
+      include: [{ model: Events}],
     });
 
     // serialize user data
     const user = userData.get({ plain: true });
 
+    // Todo: update route with where clause to only display events the user has purchased a ticket too
     // render user data on profile page
     res.render("profile", {
       ...user,
       logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Get route for saved tickets
-router.get("/profile", async (req, res) => {
-  try {
-    // retreive concerts and required attributes. Serialize concerts as plain js objects
-    // Todo: update model name
-    const savedTicketData = await savedTickets.findAll();
-
-    const savedTickets = savedTicketData.map((savedTickets) =>
-      savedTickets.get({ plain: true })
-    );
-
-    // render objects and session id in homepage handlebars template
-    res.render("profile", {
-      savedTickets,
-      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
